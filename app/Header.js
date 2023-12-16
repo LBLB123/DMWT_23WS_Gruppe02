a9eadfb7ac1b7b1fab986b271e15720004ed30bb
bluml <blumluan@gmail.com>
2023-12-16 22:51:19 +0100

    hed

--- app/Header.js
+++ app/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className={styles.header}>
             <ul>
-                <li><Link href="/">Green IT</Link></li>
+                <li><Link href="/">Green IiT</Link></li>
                 <li><Link href="#">Über uns</Link></li>
                 <li><Link href="#">Service</Link></li>
                 <li><Link href="#">Support</Link></li>

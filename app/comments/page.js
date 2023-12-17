6741aa036a9fd58553be9563806246ccacd76020
bluml <blumluan@gmail.com>
2023-12-17 14:22:41 +0100

    hedo

--- app/comments/page.js
+++ app/comments/page.js
@@ -1,36 +1,106 @@
 "use client";
 import Header from './Header';
 import Footer from './Footer';
-import {useEffect, useState} from "react";
+import {useState} from "react";
+import useSWR from 'swr'
 
-export default function Comment() {
-    const [comments, setComments] = useState([]);
+const fetcher = url => fetch(url).then(res => res.json());
 
-    useEffect(() => {
-        fetch('/api/get-comments')
-            .then((response) => response.json())
-            .then((data) => {
-                setComments(data.comments || []);
-            })
-            .catch((error) => console.error('Error fetching comments:', error));
-    }, []);
+const Comment = () => {
+    const [comment, setComment] = useState('');
+    const [username, setUsername] = useState('');
+    const {
+        data: comments,
+        isLoading,
+        error
+    } = useSWR('/api/get-comments', fetcher, {
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false
+    });
+
+    if (error) {
+        return <p>Failed to fetch</p>;
+    }
+    if (isLoading) {
+        return <p>Loading comments</p>;
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const postData = async () => {
+            const newComment = {
+                comment: comment,
+                username: username,
+            };
+
+            try {
+                const response = await fetch('/api/get-comments', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(newComment),
+                });
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+
+                const responseData = await response.text();
+                const parsedData = responseData ? JSON.parse(responseData) : null;
+
+                return parsedData;
+            } catch (error) {
+                console.error('Error fetching comments:', error);
+                return null;
+            }
+        };
+
+        postData().then(data => {
+            console.log(JSON.stringify(data));
+        });
+    }
+
 
     return (
         <div>
-            <Header />
-
             <main>
-                <h1>Comments</h1>
-                <ul>
-                    {comments.map((comment) => (
-                        <li key={comment.id}>
-                            {comment.comment} - {comment.username}
-                        </li>
-                    ))}
-                </ul>
-            </main>
+                <Header/>
+            <form onSubmit={handleSubmit}>
 
-            <Footer />
+                <div>
+                    <label htmlFor='username'>Username </label>
+                    <input
+                        id='username'
+                        type='text'
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='comment'>Comment </label>
+                    <input
+                        id='comment'
+                        type='text'
+                        value={comment}
+                        onChange={e => setComment(e.target.value)}
+                    />
+                </div>
+
+                <button type='submit'>Submit</button>
+            </form>
+            <ul>
+                {Array.isArray(comments) && comments.map((comment, index) => (
+                    <li key={index}>
+                        {comment.username} {comment.comment}
+                    </li>
+                ))}
+            </ul>
+                <Footer/>
+            </main>
         </div>
     );
-}
+};
+
+export default Comment;

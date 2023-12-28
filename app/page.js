7f02758653529bd215917c1a685242dbb37cce16
bluml <blumluan@gmail.com>
2023-12-28 21:36:22 +0100

    hedo

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import styles from "./Home.css"
 import Header from './Header';
 import Footer from './Footer';
 import Link from "next/link";
+import Image from "next/image";
+import hardware from "./img/pexels-valentine-tanasovich-2588757.jpg"
 
 
 export default function Home() {
@@ -11,19 +13,29 @@ export default function Home() {
         <Header />
 
         <main>
-            <div className="textContainer">
-                <h1 className="textContainer_h1">Dein Beitrag
-                für eine
-                bessere Welt
+            <div className={"container"}>
+                <div className="heroTextContainer">
+                    <h1 className="heroTextContainer_h1">Dein Beitrag
+                        für eine
+                        bessere Welt
 
-            </h1>
+                    </h1>
 
-                <p className="textContainer_p">
-                    Willkommen bei Hardware Recycler. Wir bieten informative Tipps und Lösungen wie Sie ihre Hardware richtig recyceln.
-                    Informieren Sie sich über unsere Dienstleistungen und werden Sie schon heute ein Teil einer nachhaltigen Zukunft.
+                    <p className="heroTextContainer_p">
+                        Willkommen bei Hardware Recycler. Wir bieten informative Tipps und Lösungen wie Sie ihre Hardware richtig recyceln.
+                        Informieren Sie sich über unsere Dienstleistungen und werden Sie schon heute ein Teil einer nachhaltigen Zukunft.
 
-                </p>
+                    </p>
+                </div>
+                <div className="heroImage-container">
+                    <Image
+                        src={hardware}
+
+                        alt="hardware"
+                    />
+                </div>
             </div>
+
 
 
             <h2>Das Elektroschrott Problem</h2>
@@ -33,13 +45,13 @@ export default function Home() {
                 Phasellus ligula massa, rhoncus a odio iaculis, luctus sagittis lectus. Curabitur fermentum leo nec rhoncus posuere. Nunc venenatis, nunc eu porttitor mollis.
             </p>
             <div className="timeline">
-                <div className="container left">
+                <div className="T_container left">
                     <div className="content">
                         <h2>Abholung</h2>
                         <p>Lorem ipsum..</p>
                     </div>
                 </div>
-                <div className="container right">
+                <div className="T_container right">
                     <div class="content">
                         <h2>Transport</h2>
                         <p>Lorem ipsum..</p>
@@ -47,13 +59,13 @@ export default function Home() {
                 </div>
             </div>
             <div className="timeline">
-                <div className="container left">
+                <div className="T_container left">
                     <div className="content">
                         <h2>Demontage</h2>
                         <p>Lorem ipsum..</p>
                     </div>
                 </div>
-                <div className="container right">
+                <div className="T_container right">
                     <div className="content">
                         <h2>Recycling</h2>
                         <p>Lorem ipsum..</p>
@@ -62,7 +74,8 @@ export default function Home() {
             </div>
             <h2>Was wir anbieten</h2>
             <p>
-                Unsere Vision ist es, eine Welt zu schaffen, in der Technologie und Umweltschutz Hand in Hand gehen. Wir setzen auf Innovation und Verantwortung, um eine nachhaltige Zukunft zu gestalten.
+                Unsere Vision ist es, eine Welt zu schaffen, in der Technologie und Umweltschutz
+                Hand in Hand gehen. Wir setzen auf Innovation und Verantwortung, um eine nachhaltige Zukunft zu gestalten.
             </p>
 
 
